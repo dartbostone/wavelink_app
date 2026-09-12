39cb65fab10a8a32bdd062b17a4ebaced96c43ab
Bostone <moturibostone@gmail.com>
2026-09-12 22:36:29 +0300

    Initial Flutter project

---
+++ README.md
@@ -0,0 +1,3 @@
+# wavelink_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/wavelink_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.wavelink_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
